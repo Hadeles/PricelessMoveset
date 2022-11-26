980f33b666f6b8a2537f4596b233070d543be23c
Hadeles_ <scottappleton7@gmail.com>
2022-11-27 11:10:26 +1300

    added icon to cover up eyesore

--- src/main/java/net/fabricmc/pricelessmoveset/CooldownView.java
+++ src/main/java/net/fabricmc/pricelessmoveset/CooldownView.java
@@ -35,30 +35,43 @@ implements HudRenderCallback {
 
 		// The bar background is 81 x 9. This includes a 1 pixel thick outline.
 		// But the bar fullness, drawn on top, is 79 x 7.
-		int barWidth = 182;
-		int barHeight = 5;
 		int textureWidth = 203;
 		int textureHeight = 81;
-		int x = client.getWindow().getScaledWidth() / 2 - barWidth / 2;  // Aligned with the armor bar
-		int y = client.getWindow().getScaledHeight() - 19;
-		if (client.player.getArmor() > 0) y = y - 10;
-		RenderSystem.setShaderColor(1f, 1f, 1f, 1f);
-		RenderSystem.setShaderTexture(0, barTexture);
-
-		// Dodge is blue
 		{
-			int u = 0;
-			int v = 50;
-			int w = (int)(dodge.getFill() * barWidth);
-			drawTexture(matrixStack, x, y, u, v, w, barHeight, textureWidth, textureHeight);
+			int barWidth = 182;
+			int barHeight = 5;
+			int x = client.getWindow().getScaledWidth() / 2 - barWidth / 2;  // Aligned with the armor bar
+			int y = client.getWindow().getScaledHeight() - 19;
+			if (client.player.getArmor() > 0) y = y - 10;
+			RenderSystem.setShaderColor(1f, 1f, 1f, 1f);
+			RenderSystem.setShaderTexture(0, barTexture);
+
+			// Dodge is blue
+			{
+				int u = 0;
+				int v = 50;
+				int w = (int)(dodge.getFill() * barWidth);
+				drawTexture(matrixStack, x, y, u, v, w, barHeight, textureWidth, textureHeight);
+			}
+
+			// Spin attack is red
+			{
+				int u = 0;
+				int v = 45;
+				int w = (int)(spinAttack.getFill() * barWidth);
+				drawTexture(matrixStack, x, y, u, v, w, barHeight, textureWidth, textureHeight);
+			}
 		}
 
-		// Spin attack is red
-		{
-			int u = 0;
-			int v = 45;
-			int w = (int)(spinAttack.getFill() * barWidth);
-			drawTexture(matrixStack, x, y, u, v, w, barHeight, textureWidth, textureHeight);
+		// While any cooldown is active, draw an icon over the XP level.
+		if (dodge.getFill() > 0.0f || spinAttack.getFill() > 0.0f) {
+			int u = 124;
+			int v = 25;
+			int w = 13;
+			int h = 15;
+			int x = client.getWindow().getScaledWidth() / 2 - (w - 1) / 2;
+			int y = client.getWindow().getScaledHeight() - 42;
+			drawTexture(matrixStack, x, y, u, v, w, h, textureWidth, textureHeight);
 		}
 	}
 }
